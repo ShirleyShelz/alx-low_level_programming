fb9bd0ac31c8f95bf66bde0d5a0d2386826ce4c7
Shirley N <nwadishelz@gmail.com>
2022-03-17 23:50:54 +0100

    Create 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+es (23 sloc) 337 Bytes
+#include "main.h"
+
+/**
+ * jack_bauer -prints every minute of the day
+ *
+ * Return: Always 0.
+ */
+
+void jack_bauer(void)
+
+{
+int a;
+int b;
+
+for (a = 0; a <= 23; a++)
+{
+for (b = 0; b <= 59; b++)
+{
+_putchar (a / 10 + '0');
+_putchar (a % 10 + '0');
+_putchar (':');
+_putchar (b / 10 + '0');
+_putchar (b % 10 + '0');
+_putchar ('\n');
+
+}
+
+
+}
+
+}
